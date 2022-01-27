8173985b1879c210fbe5b8645b3a1a30c0b88aa7
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2022-01-27 15:03:04 -0800

    Update housing nav

--- components/housingnav.tsx
+++ components/housingnav.tsx
@@ -12,21 +12,18 @@ const navigationPayroll = [
 ]
 
 function HousingNav() {
-  return <div className="bg-black">
+  return <div className="bg-black flex flex-col">
     <nav className="flex flex-row">
       {navigationPayroll.map((item, itemIdx) =>
                      
               
                      <ActiveLink activeClassName="text-gray-100 py-3 px-6 block hover:text-green-300 focus:outline-none text-green-300 border-b-2 font-medium border-green-300" href={item.url}
-                     settarget={item.newtab}
                      key={itemIdx}
-                     
                      >
-                     <a className="text-gray-600 py-3 px-6 block hover:text-green-300 focus:outline-none"
-                 
+                     <a className="text-gray-100 py-3 px-6 block hover:text-green-300 focus:outline-none underline"
+                          target={`${item.newtab === true ? "_blank" : ""}`}
                      >
                                                  {item.name}
-                                                 
                      </a>
                      </ActiveLink>
                      
